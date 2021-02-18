e629a1f4d769cee4601c9b7c3caf0333619bc211
NickSpangler <Spangleyman@mac.com>
2021-02-18 16:33:27 -0500

    start editing react app

--- demo/src/App.js
+++ demo/src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const simba = "https://i5.walmartimages.com/asr/735bc50c-9f07-4161-8d74-12130c2986d0_1.2fdb273851fa4a453c25169806bdffa9.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff"
+  const george = "https://www.littlelakecounty.com/wp-content/uploads/2016/08/curious-george-balloons.png"
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={george} className="" alt="logo" />
+        <img src={simba} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This has been edited
         </p>
         <a
           className="App-link"

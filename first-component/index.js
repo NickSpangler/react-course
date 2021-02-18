4cc60e117fa0d8a432f751086c0ee465c372c14e
NickSpangler <Spangleyman@mac.com>
2021-02-18 14:48:42 -0500

    write component as functional arrow component

--- first-component/index.js
+++ first-component/index.js
@@ -1,12 +1,21 @@
-class Hello extends React.Component {
-    render() {
-        return (
-            <div>
-            <h1>HELLO!</h1>
-            <h2>Why, Hello!</h2>
-            </div>
-        )
-    }
+// class Hello extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//             <h1>HELLO!</h1>
+//             <h2>Why, Hello!</h2>
+//             </div>
+//         )
+//     }
+// }
+
+const Hello = () => {
+    return (
+        <div>
+        <h1>HELLO!</h1>
+        <h2>Why, Hello!</h2>
+        </div>
+    )
 }
 
 ReactDOM.render(<Hello />, document.getElementById('root'))

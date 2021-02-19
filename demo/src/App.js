51dae96061d90a46e892492984539f4b58267a2e
NickSpangler <Spangleyman@mac.com>
2021-02-19 12:09:55 -0500

    clear out react app

--- demo/src/App.js
+++ demo/src/App.js
@@ -6,19 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button />
-        <Rando />
-        <img src={coolPic} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }

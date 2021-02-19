171adc687d56f02fbc9c9849f811858735ffeb4c
NickSpangler <Spangleyman@mac.com>
2021-02-19 10:21:40 -0500

    add new image source

--- pokemon-app/pokemon-app/src/components/Pokecard.js
+++ pokemon-app/pokemon-app/src/components/Pokecard.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
 import '../stylesheets/Pokecard.css'
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+
+// let threeDigitId = (id) => (id <= 999 ? `00${id}`.slice(-3) : id)
+let threeDigitId = (id) => {
+    let string = id.toString()
+    while (string.length < 3) string = '0' + string
+    return string
+}
 class Pokecard extends Component {
+
     render() {
-        const imgSrc= `${POKE_API}${this.props.id}.png`
+        const imgSrc= `${POKE_API}${threeDigitId(this.props.id)}.png`
         return (
             <div className='Pokecard'>
                 <h1 className='Pokecard-name'>{this.props.name}</h1>
